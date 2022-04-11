3bcff3ffb5b46722246abd059ac119aac29e1453
itprohelper <rudy@itprohelper.com>
2022-04-11 13:33:36 -0400

    added NLP module and defined logic of the app

--- web/app.py
+++ web/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 from flask-restful import Api, resource
 from pymongo import MongoClient
 import bcrypt
+import spacy
 
 app = Flask(__name__)
 api = Api(app)
@@ -44,4 +45,84 @@ class Register(Resource):
             "status": 200,
             "msg": "You've successfully signed up to the API"
         }
+        return jsonify(retJson)
+
+def verifyPw(username, password):
+    if not UserExist(username):
+        return False
+
+    hashed_pw = users.find({
+        "Username": username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw)==hashed_pw:
+        return True
+    else:
+        return False
+
+def countTokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Tokens"]
+    return tokens
+
+class Detect(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
+
+        if not UserExist(username):
+            retJson = {
+                "status": "301",
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status": 302,
+                "msg": "Invalid Password"
+            }
+
+        num_tokens = countTokens(username)
+
+        if num_tokens <= 0:
+            retJson = {
+                "status": 303,
+                "msg": "You're out of tokens, please refill!"
+            }
+            return jsonify(retJson)
+
+        #Calculate the edit distance
+        nlp = spacy.load('en_core_web_sm')
+
+        text1 = nlp(text1)
+        text2 = nlp(text2)
+
+        #Ratio is a number between 0 and 1 the closer to 1, the more similar
+        #text1 and text2 are.
+        ratio = text1.similarity(text2)
+
+        retJson = {
+            "status": 200,
+            "similarity": ratio,
+            "msg": "Similarity score calcualted successfully"
+        }
+
+        current_tokens = countTokens(username)
+
+        users.update({
+            "Username": username,
+        },{
+            "$set":{
+                "Tokens": current_tokens-1
+            }
+        })
+
         return jsonify(retJson)
